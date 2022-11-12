51e43a017b7d7024b327d67c7b186b84f184d3fd
TerroFlys <35329399+TerroFlys@users.noreply.github.com>
2022-11-12 22:52:36 +0100

    Transfered the Java code +- to JS

--- src/events/onPokeTwoMSG.js
+++ src/events/onPokeTwoMSG.js
@@ -1,3 +1,5 @@
+import {pokeUtility} from "../util/pokeUtility.js";
+
 export function onPokeTwoMSG(msg) {
-    msg.channel.send("Test")
+    msg.channel.send(pokeUtility(msg.content.replace("The pokémon is", '').split(" ")))
 }
