0d8ef9e85a9f9a9cc3b5a9e2b76a8a7f9e0a53d8
TerroFlys <35329399+TerroFlys@users.noreply.github.com>
2022-11-12 19:53:27 +0100

    hint command + unknown command

--- src/events/onBotMessage.js
+++ src/events/onBotMessage.js
@@ -1,9 +1,19 @@
+import {pokeUtility} from "../util/pokeUtility.js";
+
 export function onBotMessage(msg) {
     const parts = msg.content.split(" ");
     if (parts.length === 1) {
         msg.channel.send("Type 'tb! help' for more info")
         return
     }
-    if (parts[1].includes("help")) msg.reply("This bot only works with PokeTwo messages or if you have a hint from eg. another server, you can type 'tb! hint <hint>'")
+    if (parts[1].includes("help")) {
+        msg.reply("This bot only works with PokeTwo messages or if you have a hint from eg. another server, you can type 'tb! hint <hint>'");
+        return
+    }
+    if (parts[1].includes("hint")){
+        msg.reply(pokeUtility(parts.slice(2, parts.length)))
 
+        return
+    }
+    msg.reply("Unknown Command!")
 }
