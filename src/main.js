c8cbb08b5c5478e813c90ea7310af7b8057c9f9a
Chenko Mortier <35329399+TerroFlys@users.noreply.github.com>
2022-11-13 00:32:33 +0000

    added "bot version" text + Formatted main.js

--- src/main.js
+++ src/main.js
@@ -1,21 +1,22 @@
-import {ActivityFlagsBitField, Client, GatewayIntentBits} from "discord.js";
-import {onPokeTwoMSG} from "./events/onPokeTwoMSG.js";
-import {onBotMessage} from "./events/onBotMessage.js";
+import { ActivityFlagsBitField, Client, GatewayIntentBits } from "discord.js";
+import { onPokeTwoMSG } from "./events/onPokeTwoMSG.js";
+import { onBotMessage } from "./events/onBotMessage.js";
 
+const BOT_VERSION = "1.0"
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.DirectMessages] });
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setActivity(ActivityFlagsBitField.Flags.SPECTATE)
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`BOT version:  ${BOT_VERSION}`)
 });
 
 const pokeTwoID = '716390085896962058'
 const pokeTwoHintMsg = "The pokémon is"
 
-client.on("messageCreate",(msg) => {
-    if (msg.author.id === pokeTwoID && msg.content.startsWith(pokeTwoHintMsg)) onPokeTwoMSG(msg);
-    if (msg.author.id !== pokeTwoID && msg.content.startsWith("tb!")) onBotMessage(msg) ;
+client.on("messageCreate", (msg) => {
+  if (msg.author.id === pokeTwoID && msg.content.startsWith(pokeTwoHintMsg)) onPokeTwoMSG(msg);
+  if (msg.author.id !== pokeTwoID && msg.content.startsWith("tb!")) onBotMessage(msg);
 });
 
 
