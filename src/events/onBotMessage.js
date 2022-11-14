374a121db9a33bbcccd0dc4fc3f632d2f7904c0b
Chenko Mortier <35329399+TerroFlys@users.noreply.github.com>
2022-11-14 07:34:08 +0000

    fixed crash when no other input was given with hint

--- src/events/onBotMessage.js
+++ src/events/onBotMessage.js
@@ -1,19 +1,23 @@
-import {pokeUtility} from "../util/pokeUtility.js";
+import { pokeUtility } from "../util/pokeUtility.js";
 
 export function onBotMessage(msg) {
-    const parts = msg.content.split(" ");
-    if (parts.length === 1) {
-        msg.channel.send("Type 'tb! help' for more info")
-        return
+  const parts = msg.content.split(" ");
+  if (parts.length === 1) {
+    msg.channel.send("Type 'tb! help' for more info")
+    return
+  }
+  if (parts[1].includes("help")) {
+    msg.reply("This bot only works with PokeTwo messages or if you have a hint from eg. another server, you can type 'tb! hint <hint>'");
+    return
+  }
+  if (parts[1].includes("hint")) {
+    if (parts.length === 2) {
+      msg.reply("Usage: `tb! hint <hint>`\nExample: `tb! hint ____ ____`")
+      return
     }
-    if (parts[1].includes("help")) {
-        msg.reply("This bot only works with PokeTwo messages or if you have a hint from eg. another server, you can type 'tb! hint <hint>'");
-        return
-    }
-    if (parts[1].includes("hint")){
-        msg.reply(pokeUtility(parts.slice(2, parts.length)))
+    msg.reply(pokeUtility(parts.slice(2, parts.length)))
 
-        return
-    }
-    msg.reply("Unknown Command!")
+    return
+  }
+  msg.reply("Unknown Command!")
 }
